8ae130889bba8dfb4afacd0a761fa956ca28234e
Lisa MacCarrigan and Peter Armington <peter@>
2016-08-22 11:38:38 -0400

    create method for allDetail, add js to display details for any given selected task

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,11 @@ function Task(taskName, deadline, priority, details){
   this.deadline = deadline;
   this.priority = priority;
   this.details = details;
+
+}
+
+Task.prototype.allDetail = function() {
+  return "Deadline: " + this.deadline + "<br>Priority: " + this.priority + "<br>Details: " + this.details;
 }
 
 $(document).ready(function () {
@@ -16,9 +21,15 @@ $(document).ready(function () {
 
     var myTask = new Task (taskName, deadline, priority, details);
 
+
+
     $("#task-list").append("<li><span class='listedTask'>" + taskName + "</span></li>");
+    $(".listedTask").last().click(function() {
+      $(".detail-display").show();
+      $(".task-name").text(myTask.taskName);
+      $(".details").html(myTask.allDetail());
+    });
 
 
-
-  })
-})
+  });
+});
