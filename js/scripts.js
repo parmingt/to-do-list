88515fe28f09a8022b31485369324c85c0090b3f
Lisa MacCarrigan and Peter Armington <peter@>
2016-08-22 13:41:32 -0400

    store tasks in array

--- js/scripts.js
+++ js/scripts.js
@@ -11,6 +11,7 @@ Task.prototype.allDetail = function() {
 }
 
 $(document).ready(function () {
+  var taskArray = [];
 
   $("#input-form").submit(function(event){
     event.preventDefault();
@@ -22,14 +23,22 @@ $(document).ready(function () {
 
     var myTask = new Task (taskName, deadline, priority, details);
 
+    taskArray.push(myTask);
+    $("#task-table > tbody").empty();
 
-    $("#task-table").append("<tr><td><span class='glyphicon glyphicon-flash " + priority + "'></span></td><td><span class='listedTask'>" + taskName + "</span></td><td><span class='markComplete'>Mark Complete</span></td></tr>");
+    
+    taskArray.forEach(function(task){
+      $("#task-table").append("<tr><td><span class='glyphicon glyphicon-flash " + task.priority + "'></span></td><td><span class='listedTask'>" + task.taskName + "</span></td><td><span class='markComplete'>Mark Complete</span></td></tr>")
+    })
+
     $(".listedTask").last().click(function() {
       $(".detail-display").show();
       $(".task-name").text(myTask.taskName);
       $(".details").html(myTask.allDetail());
     });
 
+    $(".markComplete").last().click(function(){
 
+    })
   });
 });
